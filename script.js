4b4f16428391906fba7cc88a8d5f41f381d3d7fd
baiklahbanggg <rezayanto690@gmail.com>
2025-10-07 12:24:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementByld('SapaButton');
+const tombolSapa = document.getElementByld('sapaButton');
 tombolSapa.addEventListener('click', function() {
-alert('Halo! Terima kasih sudah berkunjung!');
-});
+  alert('Halo! Terima kasih sudah berkunjung');
+}):
